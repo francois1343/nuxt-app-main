4edc38db99a3b9b39b079b80b5f312e90bc03b83
francis <dh.fr1109@gmail.com>
2026-08-27 14:07:23 +0200

    patch motos

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,3 +3,11 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true }
 })
+
+runtimeConfig: {
+  dbHost: '77.37.35.163'
+  dbPort: 3306
+  dbUser: 'u868520261_nuxt'
+  dbName: 'u868520261_nuxt'
+  dbPassword: 'NuxtCepegra11!'
+}
